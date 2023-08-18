df6c85a33f237bf5a5b3545600f68d991d43b3ef
youllneverreachme <elmpelmom@gmail.com>
2023-08-18 09:50:53 +0200

    more html and css

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello there");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar-wrapper");
+const toggleBtn = document.querySelector(".toggle-btn");
+closeBtn.addEventListener("click", () => {
+  if (sidebar.classList.contains("show")) {
+    sidebar.classList.remove("show");
+  }
+});
+toggleBtn.addEventListener("click", () => {
+  if (!sidebar.classList.contains("show")) {
+    sidebar.classList.add("show");
+  }
+});
---
+++ js/data.js
@@ -0,0 +1,23 @@
+const navTags = [
+  {
+    tag: "new game",
+  },
+  {
+    tag: "difficulty",
+    subtags: ["easy", "normal", "hard"],
+  },
+  {
+    tag: "size of deck",
+    subtags: ["16 cards", "36 cards"],
+  },
+  {
+    tag: "themes",
+    subtags: [
+      "dark fantasy",
+      "nature",
+      "people",
+      "JavaScript frameworks",
+      "surprise me",
+    ],
+  },
+];
